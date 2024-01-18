f421d51f437c823a73cd2225bdbec64c5f1b91f9
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-01-18 20:14:20 +0430

    addevent

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const menuBtn = document.querySelector(".hide-desktop")
 const closeBtn = document.querySelector(".close-icon")
+const nav = document.querySelector(".nav")
 
 
 menuBtn.addEventListener('click', () => {
-    
+    nav.classList.toggle("close-nav")
 })
